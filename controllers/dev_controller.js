dd72a7aa72b6e823bacd03a389e590118886f42c
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-12-09 00:49:16 -0500

    made dev form, dev creation function

--- controllers/dev_controller.js
+++ controllers/dev_controller.js
@@ -1,6 +1,7 @@
 const Dev = require("../models/dev");
 const Game = require("../models/game")
 const async = require("async");
+const {body, validationResult} = require("express-validator")
 
 
 //Show full list of genres
@@ -41,3 +42,63 @@ exports.dev_details = (req,res,next) => {
         }
     )
 }
+
+//dev create on GET
+exports.dev_create_get = (req,res,next) => {
+    res.render("dev_form", {
+        title: "Create Developer Page"
+    })
+}
+
+//dev create on POST
+
+exports.dev_create_post = [
+    //validation
+    body("name", "Developer name is required")
+        .trim()
+        .isLength({min:1})
+        .escape(),
+    body("founded", "Founding date is optional")
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+    //check for errors
+    (req,res,next) => {
+        //see if errors
+        const errors = validationResult(req);
+        //make new dev object
+        const dev = new Dev({name: req.body.name, founded: req.body.founded})
+
+        if(!errors.isEmpty()) {
+            res.render("dev_form", {
+                title: "Create Developer",
+                dev,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            //data is valid
+            //check if dev exists
+            Dev.findOne({name: req.body.name}).exec((err,found_dev)=>{
+                if(err){
+                    return next(err);
+                }
+                if (found_dev) {
+                    res.redirect(found_dev.url);
+                } else {
+                    dev.save((err)=> {
+                        if(err) {
+                            return next(err)
+                        }
+                        //dev saved. redirect to dev detail page.
+                        res.redirect(dev.url);
+                    })
+                }
+            })
+        }
+
+
+
+    }
+]
+
--- routes/devs.js
+++ routes/devs.js
@@ -6,6 +6,12 @@ const dev_controller = require("../controllers/dev_controller")
 /* GET devs listing. */
 router.get('/', dev_controller.dev_list);
 
+//GET dev creation page
+router.get('/create', dev_controller.dev_create_get);
+
+//POST genre for a dev
+router.post('/create', dev_controller.dev_create_post);
+
 //GET dev details
 router.get('/:id', dev_controller.dev_details)
 
