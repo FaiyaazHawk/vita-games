10743bc1486a2c0502992616220a7c801f774e02
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-12-22 01:57:58 -0500

    tried to fix railways deployment

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 const { type } = require('os');
-const mongoDB = process.env.MONGO_STRING;
+const mongoDB = `${process.env.MONGO_STRING}`;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
